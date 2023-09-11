0c5743fa488c9507b50d7cde1e7885ab58205fe3
Riley Wilkinson <quintbro@gmail.com>
2023-09-11 15:49:55 -0600

    Finish all plots

--- BikeShareEDA.R
+++ BikeShareEDA.R
@@ -1,6 +1,13 @@
 library(vroom)
+library(tidyverse)
 library(DataExplorer)
 library(GGally)
+
+
+
+setwd("C:/Users/rileyw/Bike_Share")
+
+vroom("train.csv")
 train <- vroom("train.csv")
 
 glimpse(train)
@@ -12,16 +19,62 @@ train %>%
          weather = as_factor(weather)) -> train
 
 glimpse(train)
-trim(train)
+
 plot_intro(train)
 correlation <- plot_correlation(train)
 plot_bar(train)
 plot_histogram(train)
 pairs(train)
 
-ggpairs(train)
+ggplot(train, aes(x = temp, y = count)) +
+  geom_point() +
+  geom_smooth(method = loess)
 
-cor(train)
+View(train)
+
+train %>%
+  mutate(day_of_week = wday(datetime, label = T)) %>%
+  select(count, day_of_week, temp, season, humidity) -> new_train
+
+plot_correlation(new_train)
 
 
+boxplot <- ggplot(new_train, aes(x = day_of_week, y = count, fill = day_of_week)) +
+  geom_boxplot()
+
+cor(new_train$count, new_train$lagged, use='complete.obs')
+
+train %>%
+  mutate(lagged = lag(count, n = 168)) %>%
+  select(count, lagged, temp, season, humidity) -> new_train
+correlation <- plot_correlation(new_train, cor_args = list("use" = "pairwise.complete.obs"))
+
+
+lag_plot <- ggplot(new_train, aes(x = lagged, y = count)) +
+  geom_point() +
+  geom_smooth(method = lm) +
+  theme(aspect.ratio = 1) +
+  theme_bw() +
+  labs(
+    title = "Count vs Lagged Count",
+    y = "Count",
+    x = "Count from One Week Previous"
+  )
+  
+
+train %>%
+  mutate(hour = format(as.POSIXct(datetime), format = "%H")) %>%
+  mutate(hour = as.numeric(hour)) %>%
+  mutate(timeDay = case_when(hour >= 22 | hour <= 6 ~ "Night",
+                             hour > 6 & hour < 22 ~ "Day"))-> hour_train
+
+ggplot(hour_train, aes(x = timeDay, y = count, fill = timeDay)) +
+  geom_boxplot() +
+  theme_bw() +
+  theme(aspect.ratio = 1) +
+  labs(
+    title = "Box Plots by Time of Day",
+    x = "Time of Day",
+    y = "Count"
+  ) -> timeday_plot
 
